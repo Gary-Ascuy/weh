f14878dd9de08f3e98780e038e767af904a9b71d
unknown <Gary Ascuy@GASCUYA-DH03.jalasoft.local>
2013-09-13 09:24:43 -0400

    US: Implement basic character functionality
    - Adding select animation
    - Fixing issue related with col selection
    - Improving gitignore file

--- src/gui/GUICharacter.cpp
+++ src/gui/GUICharacter.cpp
@@ -5,8 +5,11 @@ GUICharacter::GUICharacter()
 {
     //ctor
 }
+
 int findex = 0;
 int flag_index = 0;
+int select_index = 0;
+int sselect_index = 0;
 Uint32 findex_tick = 0;
 bool moveX = false;
 bool moveY = false;
@@ -16,12 +19,18 @@ void GUICharacter::Render(SDL_Rect& viewport, ResourceManager& rm) {
     SDL_Rect sdst = { viewport.x + src.x - size/2, viewport.y - size + src.y, size, size};
     SpriteID id = { 0, 0, 0 };
     SpriteID boxId = { 7, 0, 0 };
+    SpriteID selectId = { 9, 0, 0 };
+
+    if (++select_index % 3 == 0) {
+        sselect_index++;
+    }
+    selectId.row = (sselect_index) % 10;
 
     int dx = src.x - dst.x;
     int dy = src.y - dst.y;
-    int dd = dx - dy;
 
-
+    SDL_Rect sdstf = { src.x + viewport.x - 25, viewport.y + src.y - 30, 50, 50};
+    rm.RenderCharacter(&sdstf, selectId);
 
     Uint32 aux = SDL_GetTicks();
     if (aux - findex_tick > 35) {
@@ -45,18 +54,20 @@ void GUICharacter::Render(SDL_Rect& viewport, ResourceManager& rm) {
         }
     }
 
-    if (dd > 0) {
+    if (abs(dx) > abs(dy)) {
         if (dx > 0) id.row = 1;
-        else id.row = 0;
-    } else {
-        if (dx > 0) id.row = 3;
         else id.row = 2;
+    } else {
+        if (dy > 0) id.row = 3;
+        else id.row = 0;
     }
     rm.RenderCharacter(&sdst, id);
 
     if (moveX || moveY) {
-        boxId.row = (++flag_index)%10;
+        boxId.row = (++flag_index) % 10;
         SDL_Rect sdstf = { dst.x + viewport.x - 12, viewport.y + dst.y - 48, 50, 50};
         rm.RenderCharacter(&sdstf, boxId);
     }
+
+
 }
